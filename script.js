b53fc9772a34fcc289ede62f92ce026a195d3691
LMario98 <lo-coco-mario-@hotmail.it>
2026-04-11 12:18:04 +0200

    aggiunzione di una funzione getFormValues

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 let transactions = [];
 let nextId = 1;
 
+function getFormValues(){
+    let desc = document.getElementById('desc');
+    console.log(desc)
+    let amount = parseFloat(document.getElementById('amount').value);
+    console.log(amount)
+    let type = document.getElementById('type').value;
+    console.log(type)
+        return { desc, amount, type }
+
+}
+
+console.log(getFormValues())
